fe4ba08589ac65d551abca8a533cbc7c9ada8dc4
Lucas Godoy <lucassdegodoy@hotmail.com>
2023-09-14 08:58:58 -0300

    classe posiçao criada

--- JogoDeXadrez/src/aplicacao/Programa.java
+++ JogoDeXadrez/src/aplicacao/Programa.java
@@ -1,9 +1,14 @@
 package aplicacao;
 
+import quadrojogo.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		Posicao pos = new Posicao(3, 6);
+		System.out.println(pos);
+		System.out.println("------");
 
 	}
 
