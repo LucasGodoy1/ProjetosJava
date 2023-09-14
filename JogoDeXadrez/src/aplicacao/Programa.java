efa546c269b3a2b05991cfb331d09235b0545e18
Lucas Godoy <lucassdegodoy@hotmail.com>
2023-09-14 09:24:30 -0300

    criado as classes peças e tabuleiro

--- JogoDeXadrez/src/aplicacao/Programa.java
+++ JogoDeXadrez/src/aplicacao/Programa.java
@@ -1,14 +1,14 @@
 package aplicacao;
 
 import quadrojogo.Posicao;
+import quadrojogo.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
 		Posicao pos = new Posicao(3, 6);
 		System.out.println(pos);
-		System.out.println("------");
+		Tabuleiro tab = new Tabuleiro(8, 8);
 
 	}
 
---
+++ JogoDeXadrez/src/quadrojogo/Pecas.java
@@ -0,0 +1,21 @@
+package quadrojogo;
+
+public class Pecas {
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Pecas(Tabuleiro tabuleiro) {
+
+		this.tabuleiro = tabuleiro;
+		this.posicao = null;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+	
+	
+
+}
