4d25875751e10cdd23a96e3d4cd1a56e33a55587
Lucas Godoy <lucassdegodoy@hotmail.com>
2023-09-29 04:29:45 -0300

    implementado campoteste para analise

--- campo_minado/src/modelo/Campo.java
+++ campo_minado/src/modelo/Campo.java
@@ -1,13 +1,47 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 	private final int linha;
 	private final int coluna;
-	private boolean minado;
+	
+	//padrao do tipo primitivo boolen começa falso
+	private boolean aberto; //false
+	private boolean minado; //false
+	private boolean marcado; //false
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	
 	public Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
+	
+	public boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = this.linha != vizinho.linha;
+		boolean colunaDiferente = this.coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if (deltaGeral ==1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}else if (deltaGeral ==2  && diagonal ) {
+			vizinhos.add(vizinho);
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+	
+	
 	
 	
 
