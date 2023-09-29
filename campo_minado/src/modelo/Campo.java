85740b4846b654151ef12ec58a099145d276a0c4
Lucas Godoy <lucassdegodoy@hotmail.com>
2023-09-29 03:21:42 -0300

    Classe campo e tabuleiro adicionadas ao pacote modelo

---
+++ campo_minado/src/modelo/Campo.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Campo {
+	private final int linha;
+	private final int coluna;
+	private boolean minado;
+	public Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	
+
+}
