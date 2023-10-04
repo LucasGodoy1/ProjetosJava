811cd1c3b6ffd23977b4025dd7e28fa3c5db1b8b
Lucas Godoy <lucassdegodoy@hotmail.com>
2023-10-04 02:17:40 -0300

    testes de campo

--- campo_minado/teste/br/com/cod3r/cm/CampoTeste.java
+++ campo_minado/teste/br/com/cod3r/cm/CampoTeste.java
@@ -2,21 +2,38 @@ package br.com.cod3r.cm;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import modelo.Campo;
 
 
 public class CampoTeste {
+	private Campo campo;
 	
-	private Campo campo = new Campo(3,3);
+	@BeforeEach
+	void inicialCampo() {
+		campo = new Campo(3,3);
+	}
+	
 
 	@Test
-	void vizinhoRealDistancia1() {
+	void vizinhoRealDistancia1Esquerda() {
 		Campo vizinho = new Campo(3, 2);
-		
 		boolean resultado = campo.adicionarVizinho(vizinho);
 		assertTrue(resultado);
-		
 	}
+	@Test
+	void vizinhoRealDistancia1Direita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	@Test
+	void vizinhoRealDistancia1Cima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
 }
