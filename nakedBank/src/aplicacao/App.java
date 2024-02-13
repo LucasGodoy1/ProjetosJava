674a9ebf4b2288c39c3725e29ddaee155f5f060a
Leticia Goncalves <contato.leticiabarcelos@gmail.com>
2024-02-13 14:43:18 -0300

    fiz a implementação de um método que exibi na tela

--- nakedBank/src/aplicacao/App.java
+++ nakedBank/src/aplicacao/App.java
@@ -1,14 +1,21 @@
 package aplicacao;
 
 import entidade.Opcoes;
+import logo.LogoNakedBank;
 
 public class App {
     public static void main(String[] args) {
 
     Opcoes inicio = new Opcoes();
+    
+  //  inicio.primeiroMenu();
 
-    inicio.primeiroMenu();
 
+    
+    LogoNakedBank logo = new LogoNakedBank();
+    
+    logo.mostrarLogo();
+    
 
     }
 }
