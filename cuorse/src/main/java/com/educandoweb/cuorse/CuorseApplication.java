754618d4e540d99e3bbed1fed601ca0f247b0f3d
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-02-20 12:26:14 -0300

    Iniciado projeto de web Service

---
+++ cuorse/src/main/java/com/educandoweb/cuorse/CuorseApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.cuorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuorseApplication.class, args);
+	}
+
+}
