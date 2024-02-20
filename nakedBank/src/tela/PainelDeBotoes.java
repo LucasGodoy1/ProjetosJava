95e155da96f80cdfcbf172067704aa8fe7316921
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-02-20 12:26:48 -0300

    Gerado tela inicial

--- nakedBank/src/tela/PainelDeBotoes.java
+++ nakedBank/src/tela/PainelDeBotoes.java
@@ -9,11 +9,21 @@ public class PainelDeBotoes extends JPanel {
     private final Color COR_ROXO_ESCURO = new Color(13, 3, 31);
 
     public PainelDeBotoes(){
-        setLayout(new FlowLayout());
-        setBackground(COR_ROXO_ESCURO);
-        add(new Botoes("Login", COR_ROXO));
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints constrain = new GridBagConstraints();
+        constrain.weightx = 1;
+        constrain.weighty = 1;
 
-        add(new Botoes("Cadastrar", COR_ROXO));
+
+        setLayout(layout);
+
+        constrain.gridwidth =3;
+
+
+        setBackground(COR_ROXO_ESCURO);
+        add(new Botoes("Login", COR_ROXO), constrain, 0);
+
+        add(new Botoes("Cadastrar", COR_ROXO),constrain, 0 );
         setPreferredSize(new Dimension(50, 50));
 
     }
--- nakedBank/src/tela/TelaInicial.java
+++ nakedBank/src/tela/TelaInicial.java
@@ -8,10 +8,10 @@ public class TelaInicial extends JFrame {
 @SuppressWarnings("serial")
     public TelaInicial() { //tela sendo criada, abaixo as coisas sendo adicionadas a tela
 
-
         setSize(300,500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 
 
         add(new PainelDeBotoes());
