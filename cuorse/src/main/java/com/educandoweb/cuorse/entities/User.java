f653c4b24c42c81d0354ac1cb21e1caa8d176d7f
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-02-20 21:24:15 -0300

    gerado conexão com o H2 DB

--- cuorse/src/main/java/com/educandoweb/cuorse/entities/User.java
+++ cuorse/src/main/java/com/educandoweb/cuorse/entities/User.java
@@ -1,11 +1,23 @@
+//entidade User, ela é a primeira entidade do projeto
 package com.educandoweb.cuorse.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
-//entidade User, ela é a primeira entidade do projeto
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
--- cuorse/src/main/java/com/educandoweb/cuorse/resources/UserResource.java
+++ cuorse/src/main/java/com/educandoweb/cuorse/resources/UserResource.java
@@ -1,6 +1,6 @@
 //esse é nosso recurso web correpondente a entidade User
-//esse recurso irá disponibilizar 2 end points para recuperar os usuarios cadastrados
-//e tabem um usuario retornando o ID dele.
+//esse recurso irá disponibilizar dois end points para recuperar os usuarios cadastrados
+//e tambem um usuario retornando o ID dele.
 package com.educandoweb.cuorse.resources;
 
 import org.springframework.http.ResponseEntity;
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.cuorse.entities.User;
 
-@RestController
-@RequestMapping(value = "/users")
+@RestController //controlador REST que responde no caminho /users
+@RequestMapping(value = "/users")//Definindo o caminho para o recurso, nesse caso o recurso se refere A "Users"
 public class UserResource {
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
+	//ResponseEntity é uma tipo especifico do spring para retornar respostas de requisições WEB
+	@GetMapping 
+	public ResponseEntity<User> findAll(){ //método para acessar os Usuarios
 		User u = new User(1L, "Maria", "Maria@gmail.com", "9999999", "1234");
-		return ResponseEntity.ok(u);
+		return ResponseEntity.ok().body(u); //ResponseEntity.ok para retornar a resposta com sucesso
+		//em seguida usaremos o body(OBJETO); para retornar o corpo do objeto 
 	}
 }
