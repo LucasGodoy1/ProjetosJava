d22cb2036ac27de322752e0dae106d3949f54037
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-07-05 21:11:17 -0300

    Refactor: classe Main

--- pegaVaga/src/main/java/LucasGodoy1/com/github/PegaVagaBot.java
+++ pegaVaga/src/main/java/LucasGodoy1/com/github/PegaVagaBot.java
@@ -1,10 +1,10 @@
-package LucasGodoy1.com.github.service;
+package LucasGodoy1.com.github;
 
 import LucasGodoy1.com.github.model.TelegraaAutoMsg;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
-public class TesteTelegran {
+public class PegaVagaBot {
     public static void main(String[] args) throws TelegramApiException {
         TelegraaAutoMsg bot = new TelegraaAutoMsg();
         bot.runBot();
